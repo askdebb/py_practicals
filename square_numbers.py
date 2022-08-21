e1b9c6d99f25cc58c93cfc14a3139211d328d63c
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2022-08-21 22:56:35 +0000

    still on it

--- square_numbers.py
+++ square_numbers.py
@@ -1,11 +1,28 @@
 # This program receives input form the user 
 # and square numbers that are greater than 5
 
+from tabnanny import verbose
+
+
 def check_if_int(ask_user):
-    if type(ask_user) == 'class int':
-        print("You entered an integer of {}".format(ask_user))
+    joining_verified = ask_user
+    if joining_verified <= 0 or joining_verified > 0:
+        print("You entered an integer of {}".format(joining_verified))
+        return joining_verified    
     else:
+        return joining_verified
+   
+counter = 3
+if counter > 0:
+    try:
+        ask_user = input("Enter a number: ")
+        verify_user_input = check_if_int(ask_user)
+        if verify_user_input == ask_user:
+            double_number = (ask_user * ask_user)
+            print("Double of {} is: {}.".format(ask_user,double_number))
+    except  (NameError,SyntaxError) as ne:
         print("Invalid input")
-
-
-ask_user = input("Enter a number: \n")
+    counter-1
+    if counter <= 0:
+        print("start again, programm closed")
+        
